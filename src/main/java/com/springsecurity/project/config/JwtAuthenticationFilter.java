096254ae1bb228582ec230fad66c3cd31e4e1f9a
Dixit vara <varadixit@gmail.com>
2024-03-06 18:17:55 +0530

    first commit

--- src/main/java/com/springsecurity/project/config/JwtAuthenticationFilter.java
+++ src/main/java/com/springsecurity/project/config/JwtAuthenticationFilter.java
@@ -32,7 +32,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         final String jwt;
         final String userEmail;
 
-        if(StringUtils.isEmpty(authHeader) || authHeader.startsWith("Bearer "))
+        if(StringUtils.isEmpty(authHeader) || !authHeader.startsWith("Bearer "))
         {
             filterChain.doFilter(request,response);
             return;
